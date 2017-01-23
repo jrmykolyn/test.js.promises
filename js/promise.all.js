385e518ce798651141100b8be709fa39dd6b98de
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-23 00:23:27 -0500

    Added new 'Promise.all() test: passed 2x 'rejected' promises.

--- js/promise.all.js
+++ js/promise.all.js
@@ -19,3 +19,30 @@ Promise.all( [ promiseA1, promiseA2 ] )
             console.log( error );
         }
     );
+
+
+// Test below examines the result of:
+// - passing 2x promises to `Promise.all()`;
+// - both promises are rejected;
+// - one promise is rejected immediately, and the other is rejected after 5s.
+var promiseB1 = new Promise( function( resolve, reject ) {
+    setTimeout( function() {
+        reject( 'This promise was rejected after 5s.' );
+    }, 5000 )
+} );
+
+var promiseB2 = new Promise( function( resolve, reject ) {
+    reject( 'This promise was rejected immediately from within the `executor` function.' );
+} );
+
+Promise.all( [ promiseB1, promiseB2 ] )
+    .then(
+        function( response ) {
+            console.log( 'INSIDE `resolve` HANDLER FOR SECOND `Promise.all()` INVOCATION' );
+            console.log( response );
+        },
+        function( error ) {
+            console.log( 'INSIDE `reject` HANDLER FOR SECOND `Promise.all()` INVOCATION' );
+            console.log( error );
+        }
+    );
