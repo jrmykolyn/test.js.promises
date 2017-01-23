7da3d04c71f073a12591c672771ad80503ace8da
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-23 00:04:03 -0500

    Added new 'Promise.resolve()' test: passed Promise object that is guaranteed to resolve.

--- js/promise.resolve.js
+++ js/promise.resolve.js
@@ -1,3 +1,19 @@
+var promiseWillSucceed = new Promise( function( resolve, reject ) {
+    setTimeout( function() {
+        resolve( 'This promise was automatically resolved from within the `executor` function.' );
+    }, 3000 );
+} );
+
+Promise.resolve( promiseWillSucceed )
+    .then(
+        function( response ) {
+            console.log( response );
+        },
+        function( error ) {
+            console.log( error );
+        }
+    );
+
 var promiseWillFail = new Promise( function( resolve, reject ) {
     setTimeout( function() {
         reject( 'This promise was automatically failed.' );
